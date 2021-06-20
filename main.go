bd0f903ef26b8a96c419ebce96b66af0b465f48e
soosungp33 <soosungp33@gmail.com>
2021-06-20 17:14:38 +0900

    CICD 완료

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/test", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "CICD branch test2",
+			"message": "CICD 구축 완료",
 		})
 	})
 	r.Run(":5000")
