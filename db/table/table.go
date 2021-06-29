909c21a7892c67085dbc9cd7039aec0a6075fbd9
soosungp33 <soosungp33@gmail.com>
2021-06-29 18:45:25 +0900

    cicd test

--- db/table/table.go
+++ db/table/table.go
@@ -7,7 +7,7 @@ type User struct {
 }
 
 // 테이블 이름을 따로 설정해주지 않으면 default로 구조체이름을 소문자로 만든 후 s가 붙은 테이블을 참조함
-// ex) User구조체면 users의 테이블을 참조함. 따라서 밑에 TableName처럼 테이블 이름을 설정해야함
+// ex) User구조체면 users의 테이블을 참조함. 따라서 밑에 TableName처럼 테이블 이름을 설정
 func (User) TableName() string {
 	return "User"
 }
