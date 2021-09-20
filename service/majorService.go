1ae097990d96a017efc29e1dbb7ebe24a74dbda3
soosungp33 <soosungp33@gmail.com>
2021-09-20 21:33:41 +0900

    feat [#7] : 조건에 맞는 kospi, kosdaq JSON으로 출력

--- service/majorService.go
+++ service/majorService.go
@@ -1,6 +1,9 @@
 package service
 
 import (
+	"strconv"
+	"time"
+
 	"github.com/stock-market-simulator/Go/db/table"
 )
 
@@ -10,15 +13,22 @@ type majorList struct {
 }
 
 func (g *gormHandler) GetMajorData() majorList {
+	now := time.Now()
+	year := now.Year() - 1
+	month := int(now.Month())
+	day := now.Day()
+	start := convert(year, month-2, day)
+	end := convert(year, month, day)
+
 	var kospi []*table.Kospi
 	var kosdaq []*table.Kosdaq
 
-	kospiResult := g.db.Find(&kospi)
+	kospiResult := g.db.Where("date BETWEEN ? AND ?", start, end).Find(&kospi)
 	if kospiResult.Error != nil {
 		panic(kospiResult.Error)
 	}
 
-	kosdaqResult := g.db.Find(&kosdaq)
+	kosdaqResult := g.db.Where("date BETWEEN ? AND ?", start, end).Find(&kosdaq)
 	if kosdaqResult.Error != nil {
 		panic(kosdaqResult.Error)
 	}
@@ -26,4 +36,20 @@ func (g *gormHandler) GetMajorData() majorList {
 	result := majorList{kospi, kosdaq}
 
 	return result
+}
+
+func convert(year int, month int, day int) string {
+	if month < 1 {
+		year -= 1
+		month = 12 - month
+	}
+	return toString(year) + "/" + toString(month) + "/" + toString(day)
+}
+
+func toString(num int) string {
+	if num < 10 {
+		return "0" + strconv.Itoa(num)
+	} else {
+		return strconv.Itoa(num)
+	}
 }
