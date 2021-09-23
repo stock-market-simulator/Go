6ad7abbf754f81ce8b5731b1d68f4f0305141a1c
soosungp33 <soosungp33@gmail.com>
2021-09-23 23:31:46 +0900

    fix: bookmarkResponseDto.go json 태그 수정

--- controller/dto/bookmarkResponseDto.go
+++ controller/dto/bookmarkResponseDto.go
@@ -3,5 +3,5 @@ package dto
 type BookmarkResponseDto struct {
 	Name          string `json:"name"`
 	CurrentPrice  int    `json:"currentPrice"`
-	PreviousPrice int    `json:"previousPrice`
+	PreviousPrice int    `json:"previousPrice"`
 }
